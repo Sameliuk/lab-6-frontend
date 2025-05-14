713dba2102e31370ff7955e24f64dcaa45c0feff
Sameliuk <sameliuk.anastasia@gmail.com>
2025-05-14 20:46:44 +0300

    pagination and filtration
    (cherry picked from commit dd9c640d79d52f071a5d9cc5903f46e4ac9f98e7)

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -6,12 +6,17 @@ import { useUserStore } from '@/stores/userStore';
 const userStore = useUserStore();
 const router = useRouter();
 
+// Отримуємо дані з localStorage та парсимо їх
+const userData = localStorage.getItem('user');
+const parsedUser = userData ? JSON.parse(userData) : null;
+
+// Перевірка наявності ім'я
 const accountRoute = computed(() =>
-    userStore.fname ? '/users/profile' : '/users/signIn'
+    parsedUser && parsedUser.fname ? '/users/profile' : '/users/signIn'
 );
 
 const accountLabel = computed(() =>
-    userStore.fname ? userStore.fname : 'Увійти'
+    parsedUser && parsedUser.fname ? parsedUser.fname : 'Увійти'
 );
 
 function logout() {
@@ -27,7 +32,11 @@ function logout() {
 
         <div class="profile">
             <RouterLink :to="accountRoute">{{ accountLabel }}</RouterLink>
-            <button v-if="userStore.fname" @click="logout" class="logout-btn">
+            <button
+                v-if="parsedUser && parsedUser.fname"
+                @click="logout"
+                class="logout-btn"
+            >
                 Вийти
             </button>
         </div>
